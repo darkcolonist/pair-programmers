820014c3ab064e2b3be165e46e3011885331d31b
cris <cris@nms.ph>
2023-07-25 13:48:34 +0800

    looks good and it is working

--- index.php
+++ index.php
@@ -31,6 +31,67 @@ function println($string = ''){
   echo "{$string}\n";
 }
 
+// Function to rotate the array based on $rotate value
+function rotateArrayInefficient($array, $rotate)
+{
+  $count = count($array);
+
+  if ($count === 0 || $rotate === 0) {
+    return $array;
+  }
+
+  $rotate = $rotate % $count; // In case $rotate is greater than the array size
+
+  for ($i = 0; $i < $rotate; $i++) {
+    $element = array_shift($array);
+    array_push($array, $element);
+  }
+
+  return $array;
+}
+
+// Function to rotate the array based on $rotate value
+function rotateArray($array, $rotate)
+{
+  $count = count($array);
+
+  if ($count === 0 || $rotate === 0) {
+    return $array;
+  }
+
+  $rotate = $rotate % $count; // In case $rotate is greater than the array size
+
+  $slice1 = array_slice($array, $rotate);
+  $slice2 = array_slice($array, 0, $rotate);
+
+  return array_merge($slice1, $slice2);
+}
+
+// Function to display members in a specific order based on $set value
+function getMemberPairings($names, $set)
+{
+  $count = count($names);
+
+  if ($count === 0) {
+    println("No members found.");
+    return;
+  }
+
+  $rotatedNames = rotateArray($names, $set);
+
+  $pairings = [];
+
+  for ($i = 0; $i < $count; $i += 2) {
+    $nextIndex = $i + 1;
+    $nextName = isset($rotatedNames[$nextIndex]) ? $rotatedNames[$nextIndex] : '';
+
+    $pairings[] = [$rotatedNames[$i], $nextName];
+  }
+
+  return $pairings;
+}
+
+
 // Function to display members in a specific order based on $set value
 function displayMembersBySet($names, $set)
 {
@@ -41,33 +102,34 @@ function displayMembersBySet($names, $set)
     return;
   }
 
-  if ($set === 1) {
-    for ($i = 0; $i < $count; $i += 2) {
-      $nextIndex = $i + 1;
-      $nextName = isset($names[$nextIndex]) ? $names[$nextIndex] : '';
-      println("{$names[$i]} -> $nextName");
-    }
-  } elseif ($set === 2) {
-    for ($i = $count - 1; $i >= 0; $i -= 2) {
-      $prevIndex = $i - 1;
-      $prevName = isset($names[$prevIndex]) ? $names[$prevIndex] : '';
-      println("{$names[$i]} -> $prevName");
-    }
-  } else {
-    println("Invalid set value. Please use 1 or 2.");
+  // if ($set === 1) {
+  $rotatedNames = rotateArray($names, $set);
+
+  for ($i = 0; $i < $count; $i += 2) {
+    $nextIndex = $i + 1;
+    $nextName = isset($rotatedNames[$nextIndex]) ? $rotatedNames[$nextIndex] : '';
+    println("{$rotatedNames[$i]} -> $nextName");
   }
+  // } elseif ($set === 2) {
+  //   for ($i = $count - 1; $i >= 0; $i -= 2) {
+  //     $prevIndex = $i - 1;
+  //     $prevName = isset($names[$prevIndex]) ? $names[$prevIndex] : '';
+  //     println("{$names[$i]} -> $prevName");
+  //   }
+  // } else {
+  //   println("Invalid set value. Please use 1 or 2.");
+  // }
 
 }
 
-// Path to the members.txt file
-$filename = "members.txt";
-
 // Call the function to read members from the file
-$members = readMembers($filename);
+$members = readMembers("members.txt");
 
 // Set the desired set value (1 or 2)
 $set = (integer)$_GET["set"];
 
 // Call the function to display members based on the set value
-displayMembersBySet($members, $set);
+// displayMembersBySet($members, $set);
+$pairs = getMemberPairings($members, $set);
+print_r($pairs);
 ?>
