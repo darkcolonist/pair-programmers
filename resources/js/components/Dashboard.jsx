1a8a79f9afaf07ae71ea9d719e587a1a6dd534e6
cris <cris@nms.ph>
2023-08-09 09:16:11 +0800

    #10 today table verbose data build

--- resources/js/components/Dashboard.jsx
+++ resources/js/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import { Divider, Grid, Paper, Stack, Typography } from "@mui/material";
 import React from "react";
 import Pairs from "./Pairs";
 import LoadingTable from "./LoadingTable";
+import Moment from "./Moment";
 
 const Dashboard = function () {
   const [dataLoaded,setDataLoaded] = React.useState(false);
@@ -26,9 +27,8 @@ const Dashboard = function () {
       <Stack direction="row" justifyContent="right"
         divider={<Divider orientation="vertical" flexItem />}
         spacing={2}>
-        <Typography className="footerInfoCode">kwa</Typography>
-        <Typography className="footerInfoCode">kwa</Typography>
-        <Typography className="footerInfoCode">kwa</Typography>
+        <Typography className="footerInfoCode">Pair up #{currentData.rotations}</Typography>
+        <Typography className="footerInfoCode">Generated on <Moment format="dddd, MMMM Do YYYY, h:mm:ss a">{currentData.generated}</Moment></Typography>
       </Stack>
     </Grid>
   </Grid>
