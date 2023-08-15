0153464a276707400f951f8e64193f6cec2a583e
cris <darkcolonist@gmail.com>
2023-08-15 16:43:58 +0800

    #30 internal scheduling implemented `current:increment`

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,39 +7,46 @@ use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-      \App\Console\Commands\DecrementCommand::class,
-      \App\Console\Commands\IncrementCommand::class,
-      \App\Console\Commands\DiscordSendCurrentCommand::class,
-      \App\Console\Commands\TestLogCommand::class,
-    ];
+  /**
+   * The Artisan commands provided by your application.
+   *
+   * @var array
+   */
+  protected $commands = [
+    \App\Console\Commands\DecrementCommand::class,
+    \App\Console\Commands\IncrementCommand::class,
+    \App\Console\Commands\DiscordSendCurrentCommand::class,
+    \App\Console\Commands\TestLogCommand::class,
+  ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-      $schedule->command('test:log "daily cron #1"')->dailyAt('07:00')->days([
-        Schedule::MONDAY,
-        Schedule::TUESDAY,
-        Schedule::WEDNESDAY,
-        Schedule::THURSDAY,
-        Schedule::FRIDAY,
-      ]);
-      $schedule->command('test:log "daily cron #2"')->dailyAt('07:05')->days([
-        Schedule::MONDAY,
-        Schedule::TUESDAY,
-        Schedule::WEDNESDAY,
-        Schedule::THURSDAY,
-        Schedule::FRIDAY,
-      ]);
-    }
+  /**
+   * Define the application's command schedule.
+   *
+   * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+   * @return void
+   */
+  protected function schedule(Schedule $schedule)
+  {
+    $schedule->command('current:increment"')->dailyAt('07:00')->days([
+      Schedule::MONDAY,
+      Schedule::TUESDAY,
+      Schedule::WEDNESDAY,
+      Schedule::THURSDAY,
+      Schedule::FRIDAY,
+    ]);
+    $schedule->command('test:log "daily cron #1"')->dailyAt('07:00')->days([
+      Schedule::MONDAY,
+      Schedule::TUESDAY,
+      Schedule::WEDNESDAY,
+      Schedule::THURSDAY,
+      Schedule::FRIDAY,
+    ]);
+    $schedule->command('test:log "daily cron #2"')->dailyAt('07:05')->days([
+      Schedule::MONDAY,
+      Schedule::TUESDAY,
+      Schedule::WEDNESDAY,
+      Schedule::THURSDAY,
+      Schedule::FRIDAY,
+    ]);
+  }
 }
