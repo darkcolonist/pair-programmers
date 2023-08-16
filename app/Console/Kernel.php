c7bbeec59886e9402c4de434fe60dd5fd52630bd
cris <darkcolonist@gmail.com>
2023-08-16 10:43:17 +0800

    refs #30 altered test-case

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,7 +34,7 @@ class Kernel extends ConsoleKernel
       Schedule::THURSDAY,
       Schedule::FRIDAY,
     ]);
-    $schedule->command('discord:current"')->dailyAt('10:31')->days([
+    $schedule->command('discord:current"')->dailyAt('10:44')->days([
       Schedule::MONDAY,
       Schedule::TUESDAY,
       Schedule::WEDNESDAY,
